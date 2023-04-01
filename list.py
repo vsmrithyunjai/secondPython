4c08db831b3794d0e94e3d63a4910a7118b23b8c
vsmrithyunjai <rvigpvt@gmail.com>
2023-04-01 19:14:36 -0400

    test pgm for list

---
+++ list.py
@@ -0,0 +1,23 @@
+x = object()
+y = object()
+
+# TODO: change this code
+x_list = [x] * 10
+y_list = [y] * 10
+big_list = x_list + y_list
+big_list1 = [x_list + y_list]
+print("addition ")
+print(big_list)
+print("addition list")
+print(big_list1)
+
+print("x_list contains %d objects" % len(x_list))
+print("y_list contains %d objects" % len(y_list))
+print("big_list contains %d objects" % len(big_list))
+print("big_list count is %d " % big_list.count(x))
+
+# testing code
+if x_list.count(x) == 10 and y_list.count(y) == 10:
+    print("Almost there...")
+if big_list.count(x) == 10 and big_list.count(y) == 10:
+    print("Great!")
