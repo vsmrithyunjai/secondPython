8b556234d231dfb36dba41fa46d6b400d3354caa
vsmrithyunjai <rvigpvt@gmail.com>
2023-04-01 23:12:18 -0400

    addl loop conds

--- loops.py
+++ loops.py
@@ -50,3 +50,30 @@ for x in range(10):
         continue
     print(x)
 
+
+print("Else in While While Loop2")
+print("-------------------------")
+count=0
+while(count<5):
+    print(count)
+    count +=1
+else:
+    print("count value reached %d" %(count))
+
+print ("For loop with else")
+print("-------------------")
+# Prints out 1,2,3,4
+for i in range(1, 11):
+    if(i%5==0):
+        continue
+    print(i)
+else:
+    print("%d is max" %i)
+
+
+for i in range(1, 10):
+    if(i%5==0):
+        break
+    print(i)
+else:
+    print("this is not printed because for loop is terminated because of break but not due to fail in condition")
